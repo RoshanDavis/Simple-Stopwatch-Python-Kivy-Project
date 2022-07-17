232aa92c37e1c179a4f88a6546504d37668b501d
Roshan Davis <75558109+RoshanDavis@users.noreply.github.com>
2022-07-17 17:32:14 +0530

    Created basic GUI

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+
+class MainWidget(AnchorLayout):
+    pass
+
+class Simple_StopwatchApp(App):
+    pass
+
+Simple_StopwatchApp().run()
